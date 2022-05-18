874f598a7560bea53f766c34109baf662e65fa37
MariaJoseGG <mgiraldogu@unal.edu.co>
2022-05-18 17:54:00 -0500

    Se agrega el footer

---
+++ src/Componentes_generales.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Footer} from './Footer';
+
+export function Aplicacion() {
+    return (
+        <React.Fragment>
+            <Footer/>
+        </React.Fragment>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Aplicacion} from './Componentes_generales';
 
+ReactDOM.render(
+    <React.StrictMode>
+        <Aplicacion />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
