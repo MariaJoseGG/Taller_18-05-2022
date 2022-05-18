0bfa005be99cea5df2717e02fb8acdd1e310066b
MariaJoseGG <mgiraldogu@unal.edu.co>
2022-05-18 17:28:41 -0500

    Se sube botón con el alert

---
+++ src/Componentes_generales.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Boton} from './Boton';
+
+export function Aplicacion() {
+    return (
+        <React.Fragment>
+            <Boton/>
+        </React.Fragment>
+    )
+}
