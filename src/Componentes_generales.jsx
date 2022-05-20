46620ed79890bbd8ac8c6af37eb54d59910cfa58
Tatiana Betancurth <tbetancurth@unal.edu.co>
2022-05-20 10:41:50 -0500

    se añaden tablas

---
+++ src/Componentes_generales.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Tablita} from './Tablita';
+
+export function Aplicacion(){
+    return(
+        <React.Fragment>
+            <Tablita/>
+        </React.Fragment>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import {Aplicacion} from './Componentes_generales';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+ReactDOM.render(
+    <React.StrictMode>
+        <Aplicacion />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
